5f5c00fe1c0a34e7b573866a0aa4d6f2338311ae
Khojimatov Anvarjon <khojimatov14@gmail.com>
2022-02-16 11:28:40 +0900

    Initial Commit

---
+++ bestbooks.uz/bestbooks_uzApp.swift
@@ -0,0 +1,17 @@
+//
+//  bestbooks_uzApp.swift
+//  bestbooks.uz
+//
+//  Created by Anvarjon Khojimatov on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct bestbooks_uzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
